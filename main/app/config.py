23968c6fe6992a112adbb2b88f213adf14677fff
Greg <gregobinna@gmail.com>
2019-03-23 10:22:32 +0100

    add server name

--- main/app/config.py
+++ main/app/config.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SERVER_NAME = os.getenv('SERVER_NAME', 'localhost')
     DEBUG = False
 
 
